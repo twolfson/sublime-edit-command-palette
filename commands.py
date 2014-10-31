14c0d802a138a7718224f1e3de12eb51ef5c2b58
Todd Wolfson <todd@twolfson.com>
2014-10-31 12:33:59 -0700

    Moved to file opening, seems slick enough

--- commands.py
+++ commands.py
@@ -1,5 +1,4 @@
 from os import path
-import json
 import sublime
 import sublime_plugin
 
@@ -7,29 +6,14 @@ SUBLIME_ROOT = path.normpath(path.join(sublime.packages_path(), '..'))
 COMMANDS_FILEPATH = path.join('Packages', 'User', 'Commands.sublime-commands')
 COMMANDS_FULL_FILEPATH = path.join(SUBLIME_ROOT, COMMANDS_FILEPATH)
 
-def update_settings():
-    # Load the current settings
-    # DEV: This extends User preferences on top of Default preferences
-    settings = sublime.load_settings('Preferences.sublime-settings')
-    commands = settings.get('commands', [])
-    commands_json = json.dumps(commands, indent=4)
 
-    # Write out the set of commands to our filepath
-    with open(COMMANDS_FULL_FILEPATH, 'w') as f:
-        f.write(commands_json)
+class CommandsOpenCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        """Open `Packages/User/Commands.sublime-commands` for custom definitions"""
+        # Open the User commands file
+        view = self.window.open_file(COMMANDS_FULL_FILEPATH)
 
-class CommandsEventListener(sublime_plugin.EventListener):
-    def on_post_save(self, view):
-        """
-        When we save a .sublime-settings file, re-populate `Packages/User/Commands.sublime-settings`
-
-        # DEV: Technically, we should only pay attention to `Packages/User/Preferences.sublime-settings`
-            However, that can cause more frustration than it solves
-        """
-        # If the file is not a settings file, exit early
-        filepath = view.file_name()
-        # if not filepath.endswith('.sublime-settings'):
-        #     return
-
-        # In
-        # DEV: Sublime Text does not have new Preferences loaded yet, wait for repopulation
+        # If it doesn't exist, provide a prompt
+        # TODO: For ST3, we might need to do this before opening the file
+        if not path.exists(COMMANDS_FULL_FILEPATH):
+            view.
