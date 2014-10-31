b899d5613c0f4425aa4cc69bac9561b503ba83d4
Todd Wolfson <todd@twolfson.com>
2014-10-31 15:12:43 -0700

    Started exploring using argument but realizing this is a rabbit hole

--- commands.py
+++ commands.py
@@ -10,14 +10,17 @@ COMMANDS_FULL_FILEPATH = path.join(SUBLIME_ROOT, COMMANDS_FILEPATH)
 COMMANDS_SOURCE_FULL_FILEPATH = path.abspath('default-prompt.json')
 
 class CommandsOpenCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        """Open `Packages/User/Commands.sublime-commands` for custom definitions"""
-        # If the User commands doesn't exist, provide a prompt
-        if not path.exists(COMMANDS_FULL_FILEPATH):
-            shutil.copy(COMMANDS_SOURCE_FULL_FILEPATH, COMMANDS_FULL_FILEPATH)
+    def run(self, **kwargs):
+        """Open `.sublime-commands` file for custom definitions"""
+        # If no file is provided, default to `COMMANDS_FULL_FILEPATH`
+        dest_filepath = kwargs.get('file', COMMANDS_FULL_FILEPATH)
+
+        # If the file doesn't exist, provide a prompt
+        if not path.exists(dest_filepath):
+            shutil.copy(COMMANDS_SOURCE_FULL_FILEPATH, dest_filepath)
 
         # Open the User commands file
-        view = self.window.open_file(COMMANDS_FULL_FILEPATH)
+        view = self.window.open_file(dest_filepath)
 
         # If the syntax is plain text, move to JSON
         if view.settings().get('syntax') == path.join('Packages', 'Text', 'Plain text.tmLanguage'):
