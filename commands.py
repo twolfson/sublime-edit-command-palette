c6afd487fcebadf89cf080d38efc10df485dd6a5
Todd Wolfson <todd@twolfson.com>
2014-11-01 11:33:09 -0700

    Moved to `plugin_loaded` call to fix ST3 issues

--- commands.py
+++ commands.py
@@ -3,24 +3,34 @@ import shutil
 import sublime
 import sublime_plugin
 
-SUBLIME_ROOT = path.normpath(path.join(sublime.packages_path(), '..'))
-COMMANDS_FILEPATH = path.join('Packages', 'User', 'Commands.sublime-commands')
-COMMANDS_FULL_FILEPATH = path.join(SUBLIME_ROOT, COMMANDS_FILEPATH)
+# Define dictionary for constants
+C = {}
 
-# DEV: We must use `__file__` since ST3 executes from `Packages/` instead of `Packages/Commands/`
-COMMANDS_SOURCE_FULL_FILEPATH = path.normpath(path.join(__file__, '..', 'default-prompt.json'))
+def plugin_loaded():
+    """Once the plugin has loaded, define constants"""
+    C['SUBLIME_ROOT'] = path.normpath(path.join(sublime.packages_path(), '..'))
+    C['COMMANDS_FILEPATH'] = path.join('Packages', 'User', 'Commands.sublime-commands')
+    C['COMMANDS_FULL_FILEPATH'] = path.join(C['SUBLIME_ROOT'], C['COMMANDS_FILEPATH'])
+
+    # DEV: We must use `__file__` since ST3 executes from `Packages/` instead of `Packages/Commands/`
+    C['COMMANDS_SOURCE_FULL_FILEPATH'] = path.normpath(path.join(__file__, '..', 'default-prompt.json'))
+
 
 class CommandsOpenUserCommand(sublime_plugin.WindowCommand):
     def run(self):
         """Open `Packages/User/Commands.sublime-commands` for custom definitions"""
         # If the User commands doesn't exist, provide a prompt
-        if not path.exists(COMMANDS_FULL_FILEPATH):
-            shutil.copy(COMMANDS_SOURCE_FULL_FILEPATH, COMMANDS_FULL_FILEPATH)
+        if not path.exists(C['COMMANDS_FULL_FILEPATH']):
+            shutil.copy(C['COMMANDS_SOURCE_FULL_FILEPATH'], C['COMMANDS_FULL_FILEPATH'])
 
         # Open the User commands file
-        view = self.window.open_file(COMMANDS_FULL_FILEPATH)
+        view = self.window.open_file(C['COMMANDS_FULL_FILEPATH'])
 
         # If the syntax is plain text, move to JSON
         # DEV: Syntax paths always use `/`, even on Windows via https://github.com/wbond/package_control_channel/pull/3780#issuecomment-61369387
         if view.settings().get('syntax') == 'Packages/Text/Plain text.tmLanguage':
             view.set_syntax_file('Packages/JavaScript/JSON.tmLanguage')
+
+# If we are not in ST3, run `plugin_loaded` by default
+if sublime.version() < '3000':
+    plugin_loaded()
