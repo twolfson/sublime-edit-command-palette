514eedf40f8226645d42bb6f0682a113007b3011
Todd Wolfson <todd@twolfson.com>
2014-11-01 19:19:08 -0700

    Relocating open action into separate reusable command

--- commands.py
+++ commands.py
@@ -25,21 +25,32 @@ def plugin_loaded():
     C['COMMANDS_FULL_FILEPATH'] = path.join(C['SUBLIME_ROOT'], C['COMMANDS_FILEPATH'])
 
 
-class CommandsOpenUserCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        """Open `Packages/User/Commands.sublime-commands` for custom definitions"""
-        # If the User commands doesn't exist, provide a prompt
-        if not path.exists(C['COMMANDS_FULL_FILEPATH']):
-            with open(C['COMMANDS_FULL_FILEPATH'], 'w') as f:
-                f.write(DEFAULT_CONTENT)
-
+class CommandsOpenFileCommand(sublime_plugin.WindowCommand):
+    def run(self, filepath):
+        """Open a commands file with default JSON syntax"""
         # Open the User commands file
-        view = self.window.open_file(C['COMMANDS_FULL_FILEPATH'])
+        view = self.window.open_file(filepath)
 
         # If the syntax is plain text, move to JSON
         # DEV: Syntax paths always use `/`, even on Windows via https://github.com/wbond/package_control_channel/pull/3780#issuecomment-61369387
         if view.settings().get('syntax') == 'Packages/Text/Plain text.tmLanguage':
             view.set_syntax_file('Packages/JavaScript/JSON.tmLanguage')
+
+
+class CommandsOpenUserCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        """Open `Packages/User/Commands.sublime-commands` for custom definitions"""
+        # If the User commands doesn't exist, provide a prompt
+        filepath = C['COMMANDS_FULL_FILEPATH']
+        if not path.exists(filepath):
+            with open(filepath, 'w') as f:
+                f.write(DEFAULT_CONTENT)
+
+        # Open the commands file
+        self.window.run_command('commands_open_file', {
+            'filepath': filepath,
+        })
+
 
 # If we are not in ST3, run `plugin_loaded` by default
 if sublime.version() < '3000':
